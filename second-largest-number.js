0b411102bd499aed424e5ca442d36926a212cc73
fortunatus <ihedfortune@gmail.com>
2024-02-20 05:13:52 +0100

    optimized secondlargestNumber algo

--- second-largest-number.js
+++ second-largest-number.js
@@ -21,4 +21,23 @@ const secondLargest = (arr) => {
      }
 }
 
- console.log(secondLargest([12,35,1,10,34,35,1])); 
+//  console.log(secondLargest([12,35,1,10,34,35,1])); 
+
+//optimized version
+
+const secondLargestOptimized = (arr) => {
+ let largest = Number.NEGATIVE_INFINITY;
+ let secondLargest = Number.NEGATIVE_INFINITY;
+
+ for (let i = 0; i < arr.length; i++) {
+     if (arr[i] > largest) {
+           secondLargest = largest
+      largest = arr[i]
+     }else if (arr[i] != largest && arr[i] > secondLargest) {
+      secondLargest = arr[i];
+     }
+ }
+ return secondLargest;
+}
+
+console.log(secondLargestOptimized([12,35,1,10,34,35,1]));
