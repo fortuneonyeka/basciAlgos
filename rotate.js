736c34d62aba7236d948248c40c1850edd202eb9
fortunatus <ihedfortune@gmail.com>
2024-02-21 06:52:06 +0100

    optimised algo

--- rotate.js
+++ rotate.js
@@ -6,7 +6,7 @@
 const rotateArray = (nums, k) => {
       let size = nums.length;
 
-      if (size > k) {
+      if (k > size) {
            k = k % size; 
       }
 
@@ -15,5 +15,32 @@ const rotateArray = (nums, k) => {
       return nums;
 }
 
-console.log([1,2,3,4,5,6,7]);
-console.log(rotateArray([1,2,3,4,5,6,7], 3));
+// console.log([1,2,3,4,5,6,7]);
+// console.log(rotateArray([1,2,3,4,5,6,7], 3));
+
+const rotateArrayOptimized = (nums, k) => {
+ const size = nums.length;
+
+ if (k > size) {
+      k = k % size
+ }
+ reverse(nums, 0, nums.length - 1)
+ reverse(nums, 0, k - 1)
+ reverse(nums, k, nums.length - 1)
+
+ return nums
+
+ //reverse the array : [1,2,3,4,5,6,7] --> [7,6,5,4,3,2,1]
+ //then reverse k which are the number of rotations: [7,6,5,4,3,2,1] -->> [5,6,7,4,3,2,1]
+ // reverse the remeainder of nums --> [5,6,7,1,2,3,4]
+}
+
+function reverse(nums, left, right) {
+      while (left <  right) {
+            const temp = nums[left]
+            nums[left++] = nums[right];
+            nums[right--] = temp
+      }
+}
+
+console.log(rotateArrayOptimized([1,2,3,4,5,6,7], 3));
