69aa61b64364186f1079a48231f7a3c8886f8834
fortunatus <ihedfortune@gmail.com>
2024-03-25 11:23:14 +0100

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+      const fib = [0,1]
+      for (let i = 2; i < array.length; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2]
+            
+      }
+      return fib
+}
+
+
+console.log(fibonacci(10))
+console.log(fibonacci(6))
+console.log(fibonacci(7))
