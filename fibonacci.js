a906a62d5b3b1b06982b2da80acde4a86191f6b0
fortunatus <ihedfortune@gmail.com>
2024-03-25 11:31:54 +0100

    fix loop typo error

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 function fibonacci(n) {
       const fib = [0,1]
-      for (let i = 2; i < array.length; i++) {
+      for (let i = 2; i < n; i++) {
             fib[i] = fib[i - 1] + fib[i - 2]
             
       }
