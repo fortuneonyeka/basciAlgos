6a14a10c0f658062c523ef47252ce3c7259c75cd
fortunatus <ihedfortune@gmail.com>
2024-03-25 11:32:38 +0100

    fix loop typo error

--- factorial.js
+++ factorial.js
@@ -1,6 +1,6 @@
 function factorial(n) {
       let result = 1
-      for (let i = 2; i < array.length; i++) {
+      for (let i = 2; i < n; i++) {
             result = result * i
       }
       return result
